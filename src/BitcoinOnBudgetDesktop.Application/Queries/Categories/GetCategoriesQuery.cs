61af532de6fe49dda8f12aa964ebd531126134d4
nostrgamer <notgoingtotellya@proton.me>
2025-06-03 13:08:20 -0700

    Delete src directory

--- src/BitcoinOnBudgetDesktop.Application/Queries/Categories/GetCategoriesQuery.cs
+++
@@ -1,9 +0,0 @@
-using BitcoinOnBudgetDesktop.Application.DTOs;
-using MediatR;
-
-namespace BitcoinOnBudgetDesktop.Application.Queries.Categories;
-
-/// <summary>
-/// Query to retrieve all categories for a specific budget.
-/// </summary>
-public record GetCategoriesQuery(int BudgetId) : IRequest<IEnumerable<CategoryDto>>; 
--- src/BitcoinOnBudgetDesktop.Application/Queries/Transactions/GetTransactionsQuery.cs
+++
@@ -1,9 +0,0 @@
-using BitcoinOnBudgetDesktop.Application.DTOs;
-using MediatR;
-
-namespace BitcoinOnBudgetDesktop.Application.Queries.Transactions;
-
-/// <summary>
-/// Query to get transactions for a specific budget.
-/// </summary>
-public record GetTransactionsQuery(int BudgetId) : IRequest<IEnumerable<TransactionDto>>; 
